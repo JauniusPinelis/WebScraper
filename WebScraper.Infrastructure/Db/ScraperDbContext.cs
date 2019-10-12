a853f4d75c8863d149d3bdc9f79aa0564bbe4a5b
Jaunius Pinelis <JauniusPinelis@gmail.com>
2019-10-12 15:21:33 +0300

    Finished initial db context

--- WebScraper.Infrastructure/Db/ScraperDbContext.cs
+++ WebScraper.Infrastructure/Db/ScraperDbContext.cs
@@ -2,11 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WebScraper.Infrastructure.Entities;
 
 namespace WebScraper.Infrastructure.Db
 {
     public class ScraperDbContext : DbContext
     {
+        public DbSet<JobPortalPage> PortalPages { get; set; }
+        public DbSet<PortalCategory> PortalCategories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PortalCategory>().HasData(new PortalCategory()
+            {
+                Id = 1,
+                Name = "CvOnline.lt"
+            });
+        }
     }
 }
