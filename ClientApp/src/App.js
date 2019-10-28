c5515183114f0e37e1c6bceab331f7ac0fb1aec7
Jaunius Pinelis <JauniusPinelis@gmail.com>
2019-10-28 10:35:09 +0200

    Folder fix + added/tested exios in client app

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     htmlData: []
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = () => {
+    axios
+      .get("/api/data/")
+      .then(res => {
+        this.setState({
+          htmlData: res.data
+        });
+      })
+      .catch(error => {});
+  };
+  
 }
 
 export default App;
