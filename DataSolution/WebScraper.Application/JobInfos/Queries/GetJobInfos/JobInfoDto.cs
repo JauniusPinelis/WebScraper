9c02752d478b023ff9cd52695a931fe417bccbf9
Jaunius Pinelis <JauniusPinelis@gmail.com>
2019-11-02 10:38:02 +0200

    Fixed mapping

--- DataSolution/WebScraper.Application/JobInfos/Queries/GetJobInfos/JobInfoDto.cs
+++ DataSolution/WebScraper.Application/JobInfos/Queries/GetJobInfos/JobInfoDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using WebScraper.Application.Common.Mappings;
@@ -8,5 +9,15 @@ namespace WebScraper.Application.JobInfos.Queries.GetJobInfos
 {
     public class JobInfoDto : ImapFrom<JobInfo>
     {
+        public int Id { get; set; }
+
+        public string HtmlCode { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<JobInfo, JobInfoDto>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.HtmlCode, opt => opt.MapFrom(s => s.HtmlCode));
+        }
     }
 }
