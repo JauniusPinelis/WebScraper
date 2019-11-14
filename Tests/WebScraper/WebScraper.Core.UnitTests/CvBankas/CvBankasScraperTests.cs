38b8b68df2b7c750561c3868e5ce93937b0b97d8
Jaunius Pinelis <JauniusPinelis@gmail.com>
2019-11-14 14:19:28 +0200

    Test and functionality for CvBankas Data Crawl

--- Tests/WebScraper/WebScraper.Core.UnitTests/CvBankas/CvBankasScraperTests.cs
+++ Tests/WebScraper/WebScraper.Core.UnitTests/CvBankas/CvBankasScraperTests.cs
@@ -1,14 +1,25 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using WebScraper.Core.CvBankas;
 
 namespace WebScraper.Core.UnitTests.CvBankas
 {
     [TestFixture]
     public class CvBankasScraperTests
     {
+        private readonly CvBankasScraper _scraper;
+
+        public CvBankasScraperTests()
+        {
+            _scraper = new CvBankasScraper();
+        }
+
+
         [Test]
         public void ExtractPageUrls_GivenTestData_ReturnCorrectAmountOfJobs()
         {
@@ -16,7 +27,7 @@ namespace WebScraper.Core.UnitTests.CvBankas
 
             var jobEntities = _scraper.ExtractPageUrls(textData);
 
-            jobEntities.Count().Should().Be(54);
+            jobEntities.Count().Should().Be(50);
         }
     }
 }
--- src/DataSolution/WebScraper.Core/CvBankas/CvBankasScraper.cs
+++ src/DataSolution/WebScraper.Core/CvBankas/CvBankasScraper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using WebScraper.Core.Entities;
@@ -8,6 +9,7 @@ namespace WebScraper.Core.CvBankas
 {
     public class CvBankasScraper : IScraper
     {
+
         public IEnumerable<JobInfo> ScrapeJobHtmls(IEnumerable<JobUrl> urls)
         {
             throw new NotImplementedException();
@@ -17,5 +19,49 @@ namespace WebScraper.Core.CvBankas
         {
             throw new NotImplementedException();
         }
+
+        public JobUrl ScrapeJobUrlInfo(string html)
+        {
+
+            var resultHtml = new HtmlDocument();
+            resultHtml.LoadHtml(html);
+
+            var url = resultHtml.DocumentNode.SelectSingleNode("//a[contains(@href, 'job-ad')]");
+            var salary = resultHtml.DocumentNode.SelectSingleNode("//span[contains(@class, 'salary-blue')]");
+            var location = resultHtml.DocumentNode.SelectSingleNode("//a[contains(@itemprop, 'address')]");
+            var company = resultHtml.DocumentNode.SelectSingleNode("//a[contains(@itemprop, 'name')]");
+
+            return new JobUrl()
+            {
+                Url = url?.GetAttributeValue("href", string.Empty),
+                Salary = salary?.InnerText ?? "",
+                Title = url?.InnerText,
+                Location = location?.InnerText,
+                Company = company?.InnerText
+            };
+        }
+
+        public IEnumerable<JobUrl> ExtractPageUrls(string html)
+        {
+            var results = new List<JobUrl>();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var resultNodes = htmlDocument.DocumentNode.SelectNodes("//a[contains(@class, 'list_a can_visited list_a_has_logo')]");
+
+            if (resultNodes.Count == 0)
+            {
+                return results;
+            }
+
+            foreach (var resultNode in resultNodes)
+            {
+                results.Add(ScrapeJobUrlInfo(resultNode.OuterHtml));
+            }
+
+            return results;
+        }
+
     }
 }
