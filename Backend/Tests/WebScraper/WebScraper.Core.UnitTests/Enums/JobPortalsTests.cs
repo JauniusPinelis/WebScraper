a4315151a43fdbe74d80594f856606f9cc7d25ee
JauniusPinelis <JauniusPinelis@gmail.com>
2020-04-14 16:32:36 +0300

    Refactoring to use [Description] in enums

---
+++ Backend/Tests/WebScraper/WebScraper.Core.UnitTests/Enums/JobPortalsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebScraper.Core.Enums;
+
+namespace WebScraper.Core.UnitTests.Enums
+{
+	[TestFixture]
+	public class JobPortalsTests
+	{
+		
+		[Test]
+		public void GetDescription_BasicSetup_GetsCorrectDescriptions()
+		{
+			JobPortals.CvLt.GetDescription().Should().Be("CvLt");
+			JobPortals.CvOnline.GetDescription().Should().Be("CvOnline");
+			JobPortals.CvBankas.GetDescription().Should().Be("CvBankas");
+		}
+	}
+}
